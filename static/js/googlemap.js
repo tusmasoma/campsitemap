f3d4700864e3ec5a569545fbfe75886dea468240
somak <s.k18@icloud.com>
2023-08-20 14:10:03 +0900

    リファクタリング

--- static/js/googlemap.js
+++ static/js/googlemap.js
@@ -19,172 +19,145 @@ class ControlMap {
         this.category = ["campsite","spa","conveniencestore"]
     }
     control() {
-        //this.getCurrentLocation() //現在地表示
-
-        //ローカルストレージに "results" が保存されている場合にはその値を使ってマーカーをセットし、そうでない場合には非同期処理を開始するようになっています。
-        //これにより無駄な非同期処理を減らします。
-        //一定時間以上経過している場合には再度非同期処理を行う
+        /** 
+        * 1日以内に非同期処理が行われている場合、ローカルストレージに保存された結果を使用する。
+        * それ以外の場合、非同期処理を行う。
+        */
         const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000; // 1日のミリ秒数
-        let lastProcessTime = localStorage.getItem("lastProcessTime");
+        const lastProcessTime = localStorage.getItem("lastProcessTime");
         if (lastProcessTime && Date.now() - lastProcessTime < ONE_DAY_IN_MS) {
-            // 1日以内に処理が行われているため、保存された結果を使用する
-            for(let i = 0;i < this.category.length;i++){
-                let savedResult = localStorage.getItem(`${this.category[i]}Result`);
-                if (savedResult) {
-                    savedResult = JSON.parse(savedResult);
-                    this.setMarker(savedResult); 
-                    console.log("localstorage")
-                } else {
-                    // データが保存されていない場合は再度非同期処理を行う
-                    this.spotAsynchronousprocessing(`api/${this.category[i]}/?format=json`,this.category[i])
-                    console.log("not localstorage")
-                }
-            }
+            this._useStoredDataOrFetch();
         } else {
-            // 前回の処理から1日以上経過している場合は再度非同期処理を行う
-            this.asynchronousprocessing();
-            // 現在時刻を保存する
-            localStorage.setItem("lastProcessTime", Date.now());
+            this._fetchDataAndUpdateTime();
         }
     }
-
-    asynchronousprocessing() {
-        /*
-        非同期処理を開始します
-        */
-        let self = this;
-        Promise.all([
-            self.getData('api/campsite/?format=json').then(function(result) {
-                localStorage.setItem('campsiteResult', JSON.stringify(result)); //localStorageにresultを保持することで無駄な非同期処理を減らす
-                self.setMarker(result); // for campsite
-            }),
-            self.getData('api/spa/?format=json').then(function(result) {
-                localStorage.setItem('spaResult', JSON.stringify(result)); //localStorageにresultを保持することで無駄な非同期処理を減らす
-                self.setMarker(result); // for spa
-            }),
-            self.getData('api/conveniencestore/?format=json').then(function(result) {
-                localStorage.setItem('conveniencestoreResult', JSON.stringify(result)); //localStorageにresultを保持することで無駄な非同期処理を減らす
-                self.setMarker(result); // for conveniencestore
-            })
-        ]).then(function() {
-            console.log('All data retrieved and markers set!');
-        }).catch(function(error) {
-            console.error(error);
-        });
+    _useStoredDataOrFetch() {
+        for (let category of this.category) {
+            const savedResult = localStorage.getItem(`${category}Result`);
+            if (savedResult) {
+                this.setMarker(JSON.parse(savedResult));
+            } else {
+                this._fetchSingleCategoryData(`api/${category}/?format=json`, category)
+            }
+        }
     }
-    spotAsynchronousprocessing(url,category) {
-        /**
-         単独のspotの非同期処理
-         */
-        this.getData(url).then((result) => {
+    _fetchDataAndUpdateTime() {
+        this._fetchAllCategoryData();
+        localStorage.setItem("lastProcessTime", Date.now());
+    }
+
+    _fetchAllCategoryData() {
+        const promises = this.category.map(category => this._fetchSingleCategoryData(`api/${category}/?format=json`, category));
+
+        Promise.all(promises)
+        .then(() => console.log('All data retrieved and markers set!'))
+        .catch(error => console.error('Error fetching category data:', error));
+    }
+
+    _fetchSingleCategoryData(url,category) {
+        return this._getData(url)
+        .then( result => {
+            localStorage.setItem(`${category}Result`, JSON.stringify(result)); 
             this.setMarker(result);
-            localStorage.setItem(`${category}Result`, JSON.stringify(result)); //localStorageにresultを保持することで無駄な非同期処理を減らす
-        }).catch((error) => {
-            console.error(error);
-        });
+        })
+        .catch(error => console.error(`Error fetching data for category ${category}:`, error));
     }
 
-    getData(url) {
-        return new Promise(function(resolve, reject) {
-            //XMLHttpRequestオブジェクトの作成
-            var xhttp = new XMLHttpRequest();
-            //onreadystatechangeイベントの指定(XMLHttpRequestオブジェクトのreadyStateプロパティが変化したら発火)
-            xhttp.onreadystatechange = function () {
-              //http通信のリクエスト処理が終了(readyState == 4)かつ、通信成功(status == 200)したら、
-              //通信データ(responseText)をjson形式にパースし、setMarker関数を実行する
-              if (this.readyState == 4) {
-                if (this.status == 200) {
-                  resolve(JSON.parse(this.responseText));
-                } else {
-                  reject(new Error('Failed to load data: ' + url));
-                }
-              }
-            };
-            //サーバーとの接続
-            xhttp.open('GET', url, true);
-            xhttp.setRequestHeader('Content-type', 'application/json');
-            xhttp.send();
-        });
+    _getData(url) {
+        return fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        })
+        .then( response => {
+            if (!response.ok) {
+                throw new Error(`Failed to load data from ${url}. Status: ${response.status} ${response.statusText}`);
+            }
+            return response.json();
+        })
     }
 
     setMarker(jsonObj){
         /*
         setMarker関数は引数のspotの配列をもとにgooglemap上にspotのマーカーを配置します
         */
-        // カテゴリーが`campsite`の場合、zIndexを1にする
-        const zIndex = (category) => {
-            return category === 'campsite' ? 1 : 0;
-        };
-        for(let i = 0;i < jsonObj.length;i++) {
-            //markerごとに情報ウィンドウを作成
-            if(jsonObj[i].spot_image_url.length === 0){
-                jsonObj[i].spot_image_url = [{"url":"https://thumb.ac-illust.com/39/3920178d66157451930de97cc5431a64_t.jpeg"}]
+        const DEFAULT_IMAGE_URL = "https://thumb.ac-illust.com/39/3920178d66157451930de97cc5431a64_t.jpeg";
+        
+        jsonObj.forEach(spot => {
+            if (spot.spot_image_url.length === 0) {
+                spot.spot_image_url = [{'url':DEFAULT_IMAGE_URL}];
             }
-            let infowindow =new google.maps.InfoWindow({
-                content: `<a href="spot/${jsonObj[i].id}/detail"><div class="place-window">
-                <img class="view" src="${jsonObj[i].spot_image_url[0].url}" alt="デモ画像" decoding="async" style="width: 200px; height: 150px;">
-                <div class="name">
-                <p class="body">${jsonObj[i].name}</p>
-                </div>
-                </div></a>
-                `
-            });
 
-            let marker = new google.maps.Marker({
-                position: { lat: Number(jsonObj[i].lat) , lng: Number(jsonObj[i].lng) },
-                map: this.map,
-                visible: false,
-                icon: {
-                    url: jsonObj[i].iconpath,
-                    scaledSize: new google.maps.Size(32,45.76) // 新しいサイズを指定
-                },
-                zIndex: zIndex(jsonObj[i].category),
-                name: jsonObj[i].name, // マーカーの場所名を設定する
-                category: jsonObj[i].category, //マーカーのcategory(campsite,spa,conviniencestore)を設定する
-            });
-            // 地図をズームして特定のレベルまでズームされた場合にマーカーを表示
-            let updatemarker =  _.debounce(() => {
-                const bounds = map.getBounds();
-                if(marker.getMap() && bounds.contains(marker.getPosition())){
-                    if(marker.category === 'campsite'){
-                        if(map.getZoom() >= i%7+6 || map.getZoom() >=9){
-                            marker.setVisible(true);
-                        }else{
-                            marker.setVisible(false);
-                        }
-                    }else if (marker.category === 'spa'){
-                        if (map.getZoom() >= i%15+6 || map.getZoom() >= 10) {
-                            marker.setVisible(true);
-                        } else {
-                            marker.setVisible(false);
-                        }
-                    }
-                }else{
-                    marker.setVisible(false);
-                }
-            }, 1000)
-
-            google.maps.event.addListener(map, 'dragend',updatemarker)
-            google.maps.event.addListener(map, 'zoom_changed',updatemarker)
-
-            this.bindInfoWindow(marker,infowindow)
-
+            const infoWindow = this._createInfoWindow(spot);
+            const marker = this._createMarker(spot);
+            this._bindInfoWindow(marker,infoWindow);
             this.markerList.push(marker)
-        }
-        map.setZoom(map.getZoom()) //setMarkerメソッドのデフォルトのmarkerはvisible:flaseより、zoom変化させてvisible:trueにする
+        });
+
+        this.map.setZoom(this.map.getZoom()); //setMarkerメソッドのデフォルトのmarkerはvisible:flaseより、zoom変化させてvisible:trueにする
     }
 
-    bindInfoWindow(marker,infoWindow){
-        let self = this //thisのままだと、this.currentInfoWindow の this は marker を参照する
-        //markerをクリックしたら、情報ウィンドウを閉じる
-        marker.addListener('click',function (){
-            if (self.currentInfoWindow) { // 既に開いている情報ウィンドウがある場合は閉じる
-                self.currentInfoWindow.close();
-                self.currentInfoWindow = null
+    _createInfoWindow(spot) {
+        const content = `
+        <a href="spot/${spot.id}/detail">
+            <div class="place-window">
+                <img class="view" src="${spot.spot_image_url[0].url}" alt="デモ画像" decoding="async" style="width: 200px; height: 150px;">
+                <div class="name">
+                    <p class="body">${spot.name}</p>
+                </div>
+            </div>
+        </a>
+        `
+        return new google.maps.InfoWindow({ content })
+    }
+    _createMarker(spot) {
+        const marker = new google.maps.Marker({
+            position: { lat: Number(spot.lat), lng: Number(spot.lng) },
+            map: this.map,
+            visible: false,
+            icon: {
+                url: spot.iconpath,
+                scaledSize: new google.maps.Size(32, 45.76)
+            },
+            zIndex: spot.category === 'campsite' ? 1 : 0,
+            name: spot.name,
+            category: spot.category,
+        });
+
+        const updateMarkerVisibility = _.debounce(() => {
+            const isMarkerInBounds = this.map.getBounds().contains(marker.getPosition());
+            const zoomLevel = this.map.getZoom();
+    
+            if (isMarkerInBounds) {
+                if (spot.category === 'campsite' && (zoomLevel >= spot.id % 7 + 6 || zoomLevel >= 9)) {
+                    marker.setVisible(true);
+                } else if (spot.category === 'spa' && (zoomLevel >= spot.id % 15 + 6 || zoomLevel >= 10)) {
+                    marker.setVisible(true);
+                } else {
+                    marker.setVisible(false);
+                }
+            } else {
+                marker.setVisible(false);
             }
+        }, 1000);
+
+        google.maps.event.addListener(this.map, 'dragend', updateMarkerVisibility);
+        google.maps.event.addListener(this.map, 'zoom_changed', updateMarkerVisibility);
+        
+        return marker;
+    }
+
+    _bindInfoWindow(marker,infoWindow){
+        //markerをクリックしたら、情報ウィンドウを閉じる
+        marker.addListener('click',() => {
+            if (this.currentInfoWindow) { // 既に開いている情報ウィンドウがある場合は閉じる
+                this.currentInfoWindow.close();
+                this.currentInfoWindow = null
+            }
+
             infoWindow.open(this.map, marker);
-            self.currentInfoWindow = infoWindow; // currentInfoWindowを更新
+            this.currentInfoWindow = infoWindow; // currentInfoWindowを更新
         })
+
         //情報ウィンドウの閉じるボタンをクリックしたら、localStorageのmapStateの削除
         google.maps.event.addListener(infoWindow, 'closeclick',  function() {
             infoWindow.close();
@@ -193,29 +166,31 @@ class ControlMap {
     }
 
     removeMarkers(){
-        for (let i = 0; i < this.markerList.length; i++) {
-            this.markerList[i].setMap(null);
-        }
+        this.markerList.forEach(marker => marker.setMap(null));
         this.markerList = [];
     }
 
     saveMapState() {
-        //mapの現在の状態(mapのzoom,mapのposition,情報ウィンドウの情報,情報ウィンドウに紐づくmarkerの情報)を保存する
-        localStorage.removeItem("mapState");
-        let marker = this.currentInfoWindow ? this.currentInfoWindow.getAnchor() : null;
-        let iconPath = marker ? marker.getIcon().url :null;
-        let infoWindowPosition = this.currentInfoWindow ? this.currentInfoWindow.getPosition() : null;
-        //以下のマップの状態をlocalStorageに保存しています。
-        let mapState = {
-            center: this.map.getCenter(), //マップの中心座標
-            zoom: this.map.getZoom(), //マップのズームレベル
-            marker: marker ? JSON.stringify(marker,getCircularReplacer()) : null, //現在選択されているマーカー（InfoWindowオブジェクトに紐づく）
+        //現在の情報ウィンドウがある場合のマーカーとその位置を取得
+        const marker = this.currentInfoWindow ? this.currentInfoWindow.getAnchor() : null;
+        const iconPath = marker ? marker.getIcon().url :null;
+        const infoWindowPosition = this.currentInfoWindow ? this.currentInfoWindow.getPosition() : null;
+
+        //マップの状態を保存するオブジェクトを作成
+        const mapState = {
+            center: this.map.getCenter(), 
+            zoom: this.map.getZoom(), 
+            marker: marker ? JSON.stringify(marker,getCircularReplacer()) : null, 
             iconPath: iconPath,
-            infoWindowPosition: infoWindowPosition ? JSON.stringify(infoWindowPosition,getCircularReplacer()) : null, //現在選択されているInfoWindowの座標
-            infoWindowContent: this.currentInfoWindow ? this.currentInfoWindow.getContent() : null, //現在選択されているInfoWindowの内容
+            infoWindowPosition: infoWindowPosition ? JSON.stringify(infoWindowPosition,getCircularReplacer()) : null, 
+            infoWindowContent: this.currentInfoWindow ? this.currentInfoWindow.getContent() : null, 
         };
+
+        // 以前のマップの状態を削除して新しい状態を保存
+        localStorage.removeItem("mapState");
         localStorage.setItem("mapState", JSON.stringify(mapState,getCircularReplacer()));
     }
+    
     // 5. Geolocation APIを使用して現在地を取得する
     getCurrentLocation() {
         if (navigator.geolocation) {
